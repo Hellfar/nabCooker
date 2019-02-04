16d74b1f9cb5842b87d99a12e8578394fee6e2b6
Hellfar <pierre.louchet@gmail.com>
2019-02-04 04:05:29 +0100

    Handling errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,63 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :check_params
+
+  rescue_from ActionController::RoutingError, with: :route_not_found
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::AssociationTypeMismatch, with: :request_formed_unproperly
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  before_action :set_current_user
+  def set_current_user
+    unless current_user
+      @current_user ||= doorkeeper_token ? User.find(doorkeeper_token.resource_owner_id) : nil
+    end
+  end
+  def redirect_to_safe_place
+    redirect_to(
+      if request.referer and URI(request.referer).path != request.path
+        request.referer
+      else
+        root_path
+      end)
+  end
+
+  public def route_not_found
+    flash[:alert] = "This route does not seems to lead anywhere, sorry for the inconvenience."
+    redirect_to_safe_place
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to_safe_place
+  end
+
+  def check_params
+    types = {
+      page: ActionController::Parameters,
+      filter: ActionController::Parameters,
+      sort: String
+    }
+    types.each do |parameter, type|
+      if params[parameter].present? && !(params[parameter].is_a? type)
+        raise ActionController::BadRequest
+      end
+    end
+  end
+
+  def record_not_found
+    record_message = "this record"
+    record_message = "the #{params["controller"].singularize} with id: #{params["id"]}" if params.key? "controller" and params.key? "id"
+    flash[:alert] = "Couldn't find #{record_message}."
+    redirect_to_safe_place
+  end
+
+  def request_formed_unproperly
+    errors = ActiveModel::Errors.new(nil)
+    errors.add(:app, I18n.t('errors.bad_request'))
+    render json: errors, status: :bad_request and return
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # all your other routes
+  match '*unmatched', to: 'application#route_not_found', via: :all
 end

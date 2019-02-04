ded9f5f5c8e5d7c1541a7673c195717827c61030
Hellfar <pierre.louchet@gmail.com>
2019-02-04 04:06:52 +0100

    High_voltage implementation.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def index
+    # authorize :page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # all your other routes
   match '*unmatched', to: 'application#route_not_found', via: :all
+
+  root 'pages#index'
 end
